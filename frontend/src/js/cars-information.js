656691742d318e305f1b16b44de6b3ba5e42ac4a
Ilnar Khafizov <ilnar.khafizov2019@mail.ru>
2020-03-07 13:55:58 +0400

    small fix

--- frontend/src/js/cars-information.js
+++ frontend/src/js/cars-information.js
@@ -1,30 +1,75 @@
 $(document).ready(function() {
-    PopUpHide();
+    popUpHide();
+    fillCarsOnPage();
 });
 
-function PopUpShow(type) {
-    if (type == "sedan") {
-        document.getElementById("image").setAttribute("src", "picture/sedan.png");
-        document.getElementById("brand").innerHTML = "<b>Brand:</b> Mercedes-Benz";
-        document.getElementById("model").innerHTML = "<b>Model:</b> A-Class 2019";
-        document.getElementById("color").innerHTML = "<b>Color:</b> Red";
-        document.getElementById("price").innerHTML = "<b>Price:</b> 35 000$";
-    } else if (type == "suv") {
-        document.getElementById("image").setAttribute("src", "picture/suv.png");
-        document.getElementById("brand").innerHTML = "<b>Brand:</b> Volkswagen";
-        document.getElementById("model").innerHTML = "<b>Model:</b> Tiguan";
-        document.getElementById("color").innerHTML = "<b>Color:</b> Gray";
-        document.getElementById("price").innerHTML = "<b>Price:</b> 27 000$";
-    } else if (type == "sport_car") {
-        document.getElementById("image").setAttribute("src", "picture/sport_car.png");
-        document.getElementById("brand").innerHTML = "<b>Brand:</b> Audi";
-        document.getElementById("model").innerHTML = "<b>Model:</b> R8 V10 2019";
-        document.getElementById("color").innerHTML = "<b>Color:</b> Gray";
-        document.getElementById("price").innerHTML = "<b>Price:</b> 250 000$";
-    }
+function carInformationShow(car_id) {
+    let car = getCarById(car_id);
+    document.getElementById("image").setAttribute("src", car.image);
+    document.getElementById("brand").innerHTML = "<b>Brand:</b> " + car.brand;
+    document.getElementById("model").innerHTML = "<b>Model:</b> " + car.model;
+    document.getElementById("color").innerHTML = "<b>Color:</b> " + car.color;
+    document.getElementById("price").innerHTML = "<b>Price:</b> " + car.price;
     $("#carInformation").show();
 }
 
-function PopUpHide() {
+function createCarShow(category) {
+    document.getElementById("setCategory").setAttribute("value", category);
+    $("#createCar").show();
+}
+
+function popUpHide() {
     $("#carInformation").hide();
+    $("#createCar").hide();
+}
+
+function fillCarsOnPage() {
+    let cars = getCars();
+    cars.forEach(function(car, i, cars) {
+        for (let j = 0; j < 6 - i; j++) {
+            cars.push(car);
+        }
+    });
+    cars.forEach(function(car, i) {
+        addCarOnPage(car, i);
+    });
+    document.body.setAttribute("class", JSON.stringify(cars));
+}
+
+function getCars() {
+    let json = document.body.getAttribute("class"); // cars are stored here as json
+    return JSON.parse(json);
+}
+
+function getCarById(car_id) {
+    let cars = getCars();
+    return cars[car_id];
+}
+
+function createCar() {
+    let car = {
+        "image": document.getElementById("setImage").value,
+        "brand": document.getElementById("setBrand").value,
+        "model": document.getElementById("setModel").value,
+        "color": document.getElementById("setColor").value,
+        "price": document.getElementById("setPrice").value,
+        "category": document.getElementById("setCategory").value
+    };
+    let cars = getCars();
+    cars.push(car);
+    addCarOnPage(car, cars.length - 1);
+    document.body.setAttribute("class", JSON.stringify(cars));
+    popUpHide();
+}
+
+function addCarOnPage(car, car_id) {
+    let a = document.createElement('a');
+    let img = document.createElement('img');
+    img.setAttribute("class", "category-img");
+    img.setAttribute("src", car.image);
+    a.append(img);
+    a.setAttribute("id", car_id);
+    a.setAttribute("href", "##");
+    a.setAttribute("onclick", "carInformationShow(\"" + a.id + "\")");
+    document.getElementById(car.category).before(a);
 }
